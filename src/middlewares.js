f649b08a7e270fd4aa7e4599852557e953d87cac
SonSETO <130989795+SonSETO@users.noreply.github.com>
2024-03-14 01:43:39 +0900

    server

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,56 @@
 import multer from "multer";
 
+import multerS3 from "multer-s3";
+import { S3Client } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+  region: "ap-northeast-2",
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const s3ImageUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-ai",
+  acl: "public-read",
+  key: function (request, file, ab_callback) {
+    const newFileName = Date.now() + "-" + file.originalname;
+    const fullPath = "images/" + newFileName;
+    ab_callback(null, fullPath);
+  },
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-ai",
+  acl: "public-read",
+  key: function (request, file, ab_callback) {
+    const newFileName = Date.now() + "-" + file.originalname;
+    const fullPath = "videos/" + newFileName;
+    ab_callback(null, fullPath);
+  },
+});
+
+//... localsMiddleware, protectorMiddleware, publicOnlyMiddleware ... //
+
+export const avatarUpload = multer({
+  dest: "uploads/avatars/",
+  limits: {
+    fileSize: 3 * 1000 * 1000, //3 megabyte
+  },
+  storage: s3ImageUploader,
+});
+
+export const videoUpload = multer({
+  dest: "uploads/videos/",
+  limits: {
+    fileSize: 10 * 1000 * 1000, //10 megabyte
+  },
+  storage: s3VideoUploader,
+});
+
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "wetube";
@@ -28,18 +79,18 @@ export const publicOnlyMiddleware = (req, res, next) => {
 };
 
 // const multer = require("multer");
-export const avatarUpload = multer({
-  dest: "uploads/avatars/",
-  limits: {
-    fileSize: 3000000,
-  },
-});
-export const videoUpload = multer({
-  dest: "uploads/videos/",
-  limits: {
-    fileSize: 10000000,
-  },
-});
+// export const avatarUpload = multer({
+//   dest: "uploads/avatars/",
+//   limits: {
+//     fileSize: 3000000,
+//   },
+// });
+// export const videoUpload = multer({
+//   dest: "uploads/videos/",
+//   limits: {
+//     fileSize: 10000000,
+//   },
+// });
 
 /*
 
